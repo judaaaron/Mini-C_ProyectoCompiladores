81b41527cd90023c225175cf7527e68f8609b4a2
judaaaron <judaponce14@unitec.edu>
2022-09-19 15:21:13 -0600

    correccion de iterador

---
+++ Mini-C/Pruebas/CodigoBueno#3.c
@@ -0,0 +1,63 @@
+int var (int a, int b);
+char* var2 (int a, int b, int*);
+int* var (int a, int b, int*);
+char var (int a, int b, int*);
+
+int a, b, c=5;
+
+int main (){
+    int z,b,c,h=100, value;
+    printf("%d", hola);
+    if(a > b){
+        int a = fac(a,b);
+        a = fac(a,b,c);
+	    
+    }
+return value;
+}
+
+
+
+int* var (int a, int b, int*){
+    int a,b,c,d=100;
+    char value, formato ='c', acum;
+
+    if(a > b){
+        int a = fac(a,b);
+        a = fac(a,b,c);
+	    for(int i = 0 ; 15 > a ; i+=2){
+		    while( h > 8){
+                a--;
+                printf("%d", hola);
+	            printf("%c", hola);
+	            scanf("%c", &b);
+		    }
+	    }
+    }
+return 'a';
+}
+
+int var (int a, int b){
+int z,b,c,h=100;
+char* k;
+int* punter;
+printf("%d", hola);
+printf("%c", hola);
+scanf("%c", &b);
+
+if(a > b || h <= 100){
+    int a = fac(a,b);
+    a = fac(a,b,c);
+	for(int i = 0 ; 15 > a ; i+=2){
+		while( h > 8){
+            a++;
+            printf("%d", a);
+            printf("%c", b);
+		}
+	}
+
+    printf("Ingrese un numero: ");
+    scanf("%d", &b);
+}
+return 100;
+}
